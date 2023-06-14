b99249192174cf355cee8ea7dded8a3f207cfe1d
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-14 14:11:41 +0530

    B-2

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var a =10;
+var b =21;
+
+// //Arithmetic Operators
+console.log(a+b); // Addition Operator
+console.log(b-a); // Subtraction Operator
+console.log(a*b); // Multiplication Operator
+console.log(b/a); // Division Operator
+console.log(b%a); // Modulus Operator
+
+// Increment/Decrement Operators E.g:
+var c = 5;
+var d = c++ // Post Increment Operator
+console.log(c);
+
+var f = 7;
+var e = ++f // Pre Increment Operator
+console.log(e);
+
+// Assignment Operators e.g:
+var a = 10;
+a += 3; // Addition Assignment Operator '+='
+a -= 2; // Subtraction Assignment Operator '-='
+
+console.log(a);
+
